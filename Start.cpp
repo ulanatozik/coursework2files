597dd2196b518f4154616db3ad0a7629ca02c039
ulanatozik <159810554+ulanatozik@users.noreply.github.com>
2024-11-15 20:07:32 +0300

    Create Start.cpp

---
+++ Start.cpp
@@ -0,0 +1,21 @@
+//
+//  Source.cpp
+//  testcoursework2
+//
+//  Created by Ульяна Тозик on 14.11.24.
+//
+
+
+ 
+#include "WorkServices.h"
+#include "Furniture.h"
+#include "AccountsWork.h"
+#include "MenuTable.h"
+#include "Start.h"
+
+int main() {
+    Menu menuObj;
+    setlocale(0, "rus");
+    menuObj.startMenu();
+}
+
