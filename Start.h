4e90d3a42888e2ceeb00d634ac57cceaa2179d70
ulanatozik <159810554+ulanatozik@users.noreply.github.com>
2024-11-15 20:07:16 +0300

    Create Start.h

---
+++ Start.h
@@ -0,0 +1,30 @@
+//
+//  Start.h
+//  coursework2
+//
+//  Created by Ульяна Тозик on 15.11.24.
+//
+
+#ifndef START_H
+#define START_H
+ 
+
+#include <stdio.h>
+ 
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <unistd.h>
+#include <termios.h>
+#include <functional>
+#include <locale.h>
+#include <curses.h>
+#include <memory>
+
+using namespace std;
+
+#endif // Header_h
